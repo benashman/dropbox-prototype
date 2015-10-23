44491c1567b5fb5ee9f956804d42748d8d043792
Ben Ashman <ben.ashman@me.com>
2015-10-22 18:38:40 -0700

    view terms before create

---
+++ dropbox-prototype/CreateAccountViewTermsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CreateAccountViewTermsViewController.swift
+//  dropbox-prototype
+//
+//  Created by Ben Ashman on 10/22/15.
+//  Copyright © 2015 Ben Ashman. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewTermsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didTapDoneButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
