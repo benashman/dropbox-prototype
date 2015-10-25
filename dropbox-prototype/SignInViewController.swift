2f49290b01000b5c6c0d021a6120279a8c134379
Ben Ashman <ben.ashman@me.com>
2015-10-25 15:35:39 -0700

    add back button support to sign in form

---
+++ dropbox-prototype/SignInViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SignInViewController.swift
+//  dropbox-prototype
+//
+//  Created by Ben Ashman on 10/25/15.
+//  Copyright © 2015 Ben Ashman. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didTapBackButton(sender: AnyObject) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+}
