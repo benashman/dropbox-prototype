20d6527c521dcf66bddeeb900545cb1691d25531
Ben Ashman <ben.ashman@me.com>
2015-10-25 15:10:23 -0700

    show password strength indicator

--- dropbox-prototype/CreateAccountStepOneViewController.swift
+++ dropbox-prototype/CreateAccountStepOneViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class CreateAccountStepOneViewController: UIViewController {
     
-    
     @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +26,27 @@ class CreateAccountStepOneViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onPasswordChange(sender: AnyObject) {
+        let passwordLength = passwordTextField.text!.characters.count
+        
+        let noPassword    = UIImage(named: "create_account")
+        let weakPassword  = UIImage(named: "create1")
+        let sosoPassword  = UIImage(named: "create2")
+        let goodPassword  = UIImage(named: "create3")
+        let greatPassword = UIImage(named: "create4")
+        
+        if passwordLength == 0 {
+            imageView.image = noPassword
+        } else if passwordLength > 0 && passwordLength < 4 {
+            imageView.image = weakPassword
+        } else if passwordLength >= 5 && passwordLength < 8 {
+            imageView.image = sosoPassword
+        } else if passwordLength >= 9 && passwordLength < 12 {
+            imageView.image = goodPassword
+        } else if passwordLength >= 12 {
+            imageView.image = greatPassword
+        }
+    }
     
     @IBAction func didTapBackButton(sender: AnyObject) {
         self.navigationController?.popViewControllerAnimated(true)
